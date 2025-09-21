0e20c641436163649ac393ea5e629d1e16f06108
victor20-tech <victoradomako10@gmail.com>
2025-09-21 17:42:36 +0000

    feat(task_1): Build teacher interface

---
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -0,0 +1,19 @@
+// Define the Teacher interface
+interface Teacher {
+  readonly firstName: string;
+  readonly lastName: string;
+  fullTimeEmployee: boolean;
+  yearsOfExperience?: number; // Optional property
+  location: string;
+  [key: string]: any; // Index signature for additional properties
+}
+
+const teacher3: Teacher = {
+  firstName: 'John',
+  fullTimeEmployee: false,
+  lastName: 'Doe',
+  location: 'London',
+  contract: false, // This is an additional property allowed by the index signature
+};
+
+console.log(teacher3);
