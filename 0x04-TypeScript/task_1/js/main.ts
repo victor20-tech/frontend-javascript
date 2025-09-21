36f37fce8c67fb1f55c1411833eee6f961d54483
victor20-tech <victoradomako10@gmail.com>
2025-09-21 21:04:21 +0000

    feat: Implement printTeacher function and interface

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -11,6 +11,11 @@ interface Directors extends Teacher {
   numberOfReports: number;
 }
 
+interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
+
 const teacher3: Teacher = {
   firstName: 'John',
   fullTimeEmployee: false,
@@ -20,3 +25,18 @@ const teacher3: Teacher = {
 };
 
 console.log(teacher3);
+
+const director1: Directors = {
+  firstName: 'John',
+  lastName: 'Doe',
+  location: 'London',
+  fullTimeEmployee: true,
+  numberOfReports: 17,
+};
+
+console.log(director1);
+
+let myPrintTeacher: printTeacherFunction = function(firstName: string, lastName: string): string {
+    return `${firstName.charAt(0)}. ${lastName}`;
+};
+
