3cc8cf38983d397abf08dd0c5798523d3e36ec30
victor20-tech <victoradomako10@gmail.com>
2025-09-28 15:35:04 -0400

    feat: Implement employee-specific functions with type predicate

---
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -0,0 +1,52 @@
+interface DirectorInterface {
+  workFromHome(): string;
+  getCoffeeBreak(): string;
+  workDirectorTasks(): string;
+}
+
+interface TeacherInterface {
+  workFromHome(): string;
+  getCoffeeBreak(): string;
+  workTeacherTasks(): string;
+}
+
+class Director implements DirectorInterface {
+  workFromHome = () => 'Working from home';
+  getCoffeeBreak = () => 'Getting a coffee break';
+  workDirectorTasks = () => 'Getting to director tasks';
+}
+
+class Teacher implements TeacherInterface {
+  workFromHome = () => 'Cannot work from home';
+  getCoffeeBreak = () => 'Cannot have a break';
+  workTeacherTasks = () => 'Getting to work';
+}
+
+function createEmployee(salary: number | string): Director | Teacher {
+  if (typeof salary === 'number' && salary < 500) {
+    return new Teacher();
+  }
+  return new Director();
+}
+
+type Employee = Director | Teacher;
+
+
+function isDirector(employee: Employee): employee is Director {
+  return (employee as Director).workDirectorTasks !== undefined;
+}
+
+function executeWork(employee: Employee): void {
+  if (isDirector(employee)) {
+    console.log(employee.workDirectorTasks());
+  } else {
+    console.log(employee.workTeacherTasks());
+  }
+}
+
+
+console.log("Testing with a salary of 200 (should be a Teacher):");
+executeWork(createEmployee(200));
+
+console.log("\nTesting with a salary of 1000 (should be a Director):");
+executeWork(createEmployee(1000));
