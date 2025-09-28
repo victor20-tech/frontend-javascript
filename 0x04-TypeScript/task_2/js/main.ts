d0413be531ade6bd1d4bff398650ac9d8b054da8
Victor Adomako <victoradomako10@gmail.com>
2025-09-28 11:48:49 -0400

    Update main.ts

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -22,31 +22,31 @@ class Teacher implements TeacherInterface {
   workTeacherTasks = () => 'Getting to work';
 }
 
-function createEmployee(salary: number | string): Director | Teacher {
+type Employee = Director | Teacher;
+
+function createEmployee(salary: number | string): Employee {
   if (typeof salary === 'number' && salary < 500) {
     return new Teacher();
   }
   return new Director();
 }
 
-type Employee = Director | Teacher;
 
-
-function isDirector(employee: Employee): employee is Director {
+export function isDirector(employee: Employee): employee is Director {
   return (employee as Director).workDirectorTasks !== undefined;
 }
 
-function executeWork(employee: Employee): void {
+
+function executeWork(employee: Employee): string {
   if (isDirector(employee)) {
-    console.log(employee.workDirectorTasks());
+    return employee.workDirectorTasks(); 
   } else {
-    console.log(employee.workTeacherTasks());
+    return employee.workTeacherTasks(); 
   }
 }
 
-
 console.log("Testing with a salary of 200 (should be a Teacher):");
-executeWork(createEmployee(200));
+console.log(executeWork(createEmployee(200)));
 
 console.log("\nTesting with a salary of 1000 (should be a Director):");
-executeWork(createEmployee(1000));
+console.log(executeWork(createEmployee(1000)));
